4092cafbc8a252ce3d2d10cf968d64498e36e337
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-21 15:12:27 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте значение N. Напишите программу, которая выведет 
+// все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+void PrintNum(int n)
+{
+    Console.Write(n);
+    if (n > 1)
+    {
+        PrintNum((n-1));
+        
+    }
+}
+
+PrintNum(3);
