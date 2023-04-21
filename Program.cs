7b67776145c8ca1d7cfd2ee565a87496da7ce1c5
Nick Goodwill <nick.goodwill@yandex.ru>
2023-04-21 19:13:27 +0300

    akkerman

--- Program.cs
+++ Program.cs
@@ -4,17 +4,17 @@
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
 
-// void PrintNum(int n)
-// {
-//     Console.Write(n);
-//     if (n > 1)
-//     {
-//         PrintNum((n-1));
+void PrintNum(int n)
+{
+    Console.Write(n);
+    if (n > 1)
+    {
+        PrintNum((n-1));
         
-//     }
-// }
+    }
+}
 
-// PrintNum(3);
+PrintNum(3);
 
 // Задайте значения M и N. Напишите программу, 
 // которая найдёт сумму натуральных элементов в промежутке от M до N.
@@ -37,3 +37,32 @@ int Summ(int n, int m)
 }
 
 Console.WriteLine(Summ(1, 5));
+
+// Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+//Даны два неотрицательных числа m и n.
+//m = 2, n = 3 -> A(m,n) = 9
+//m = 3, n = 2 -> A(m,n) = 29
+
+
+Console.Write("Введите значение M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int akkerman(int m, int n)
+{
+if (m == 0) 
+{
+    return n + 1;
+}
+else if (n == 0) 
+{
+    return akkerman(m - 1, 1);
+}
+else 
+{
+    return akkerman(m - 1, akkerman(m, n - 1));
+}
+}
+
+Console.Write($"Функция Аккермана равно {akkerman(m, n)} ");
